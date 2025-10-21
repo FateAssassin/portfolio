17093746b5669d9f1d2438bfc03a5b3c648a2789
Muaz Ugur <muazugur@gmail.com>
2025-10-21 18:03:19 +0200

    Migrated to vite

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Muaz' Portfolio made with React
-- Download files and libraries with `npm install` and run `npm start` in the main folder
+- Download files and libraries with `npm install` and run `npm run dev` in the main folder
 
 ## Visit on [muazugur.com](https://www.muazugur.com)
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-import { SpeedInsights } from '@vercel/speed-insights/react';
-import { Analytics } from "@vercel/analytics/react";
-
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import NavBar from './pages/Navbar';
@@ -18,8 +15,6 @@ function App() {
 
   return (
     <div>
-      <SpeedInsights />
-      <Analytics />
       <ThemeProvider>
         <NavBar />
         <Blobs />
